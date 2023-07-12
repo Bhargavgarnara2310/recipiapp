45caa372825c46b0dc5df7514fdaa1cdaac7f867
Bhargav Garnara <bgarnara481@rku.ac.in>
2023-07-12 15:29:05 +0530

    mrege2

--- backend/index.js
+++ backend/index.js
@@ -13,33 +13,3 @@ server.use(middlewares);
 server.use(router);
 console.log(port)
 server.listen(port);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// server.use('/',function (req, res, next) {
-
-//     res.header('Access-Control-Allow-Origin', '*');
-
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     res.header('Access-Control-Allow-Credentials', false);
-
-//     next();
-// });
